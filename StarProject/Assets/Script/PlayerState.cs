2c0a76d7bb559444de9ecc03fde1fcf9fda0aaac
CP5S25 <yokohama.vcp@gmail.com>
2019-02-22 17:55:06 +0900

    Add Unity ProBuilder. Add PlayerState.cs.

---
+++ StarProject/Assets/Script/PlayerState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState {
+    protected PlayerController playerController;
+
+    public PlayerState(PlayerController playerController)
+    {
+        this.playerController = playerController;
+    }
+
+    public virtual void Start()
+    {
+
+    }
+
+	public virtual void Update()
+    {
+
+    }
+
+    public virtual void ActionBottonDown()
+    {
+
+    }
+
+    public virtual void ActionBottonUp()
+    {
+
+    }
+
+    public virtual void ActionBotton()
+    {
+
+    }
+
+    public virtual void Exsit()
+    {
+
+    }
+}
