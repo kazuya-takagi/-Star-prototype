dc357132ea2a6bee9fde357b2108f02daec13c52
CP5S37 <kaito.mitani.0223@gmail.com>
2019-03-05 16:24:39 +0900

    new Scenes

---
+++ StarProject/Assets/Script/Playerpower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//途中
+public class Playerpower : MonoBehaviour {
+
+	// Use this for initialization
+	private void OnTriggerEnter(Collider other)
+    {
+		if(other.gameObject.tag == "UnityChan2D") {
+            GameObject handgrip = transform.FindChild("Cube").gameObject;
+
+                Cube.transform.Rotate(0, 180, 0);
+
+            Cube.transform.Translate(0, -1, 0);
+
+            Cube.transform.parent = handgrip.transform;
+
+            bulletruncher.GetCube();
+        }
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
