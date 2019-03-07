76827a0c50cdfd98c98a7a406a9717c28e5b3314
CP5S25 <yokohama.vcp@gmail.com>
2019-03-07 16:08:59 +0900

    [add] Add new Scenes/Libra.unity.
    Add new Scenes/Libra.unity.
    Add LibFairy.cs for AssistFairy move.

---
+++ StarProject/Assets/Script/Libra/LibFairy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibFairy : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 fairyPosi;
+    public float distanceX;
+    public float distanceY;
+    public float distanceZ;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateFairyPosi();
+	}
+
+    private void UpdateFairyPosi()
+    {
+        fairyPosi = player.transform.position;
+        fairyPosi.x += distanceX;
+        fairyPosi.y += distanceY;
+        fairyPosi.z += distanceZ;
+
+        this.transform.position = fairyPosi;
+    }
+
+}
