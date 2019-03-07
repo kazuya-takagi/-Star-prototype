598d26749bf86b2e00a5472b0d084162ace17253
CP5S25 <yokohama.vcp@gmail.com>
2019-03-07 17:25:12 +0900

    [update] Update PlayerController.cs & PlayerModeState.cs and more
    Update PlayerController.cs
    Update PlayerModeState.cs
    Update Scenes/Libra.unity
    Add ChangeMode.cs
    Add LibraModeState.cs

---
+++ StarProject/Assets/Script/Libra/LibraModeState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibraModeState : PlayerModeState {
+    
+
+    public LibraModeState(PlayerController playerController) : base(playerController)
+    {
+        
+    }
+    public override void Start()
+    {
+        
+    }
+    public override void UpDate()
+    {
+
+    }
+    public override void ActionButton()
+    {
+        
+    }
+
+    public override void ActionButtonDown()
+    {
+        
+    }
+
+    public override void ActionButtonUp()
+    {
+        
+    }
+    
+}
