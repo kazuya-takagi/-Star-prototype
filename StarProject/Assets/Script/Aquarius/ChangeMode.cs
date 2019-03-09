80708e541ef69604d7cf1ac71b905119d0780393
CP5S27 <yokoham.vcp@gmail.com>
2019-03-09 17:16:10 +0900

    [update] Update Aquarius.unity
    Add Materials.meta
    Add Animations.meta
    Add Script/Aquarius.meta

---
+++ StarProject/Assets/Script/Aquarius/ChangeMode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarProject.Aquarius
+{
+
+    public class ChangeMode : MonoBehaviour
+    {
+
+        public PlayerController playerController;
+
+        // Use this for initialization
+        void Start() {
+
+            playerController.ChangePlayerMode(new AquariusModeState(playerController));
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
