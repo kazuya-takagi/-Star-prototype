d270280a017223785d0f364e6016e99603ba5f76
CP5S27 <yokoham.vcp@gmail.com>
2019-03-09 17:41:50 +0900

    [Update] Aquarius.unity
    Add Aquarius/AssistFairy/ParticleSystem

--- StarProject/Assets/Script/Aquarius/WaterAnimation.cs
+++ StarProject/Assets/Script/Aquarius/WaterAnimation.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterAnimation : MonoBehaviour {
+namespace StarProject.Aquarius
+{
 
-    private Animator animator;
-
-	// Use this for initialization
-	void Start () {
-        animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void OnTriggerEnter(Collider collision)
+    public class WaterAnimation : MonoBehaviour
     {
-        animator.SetBool("Move", true);
+
+        private Animator animator;
+
+        // Use this for initialization
+        void Start() {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        private void OnTriggerEnter(Collider collision) {
+            animator.SetBool("Move", true);
+        }
     }
 }
