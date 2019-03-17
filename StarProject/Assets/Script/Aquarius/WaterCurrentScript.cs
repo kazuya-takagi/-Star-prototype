70c107bc63c8ff2c4700f4cb65021584240297f0
CP5S28 <yokohama.vcp@gmail.com>
2019-03-17 15:03:17 +0900

    [Update] Aquarius
    Add WaterCurrentScript.cs

---
+++ StarProject/Assets/Script/Aquarius/WaterCurrentScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCurrentScript : MonoBehaviour
+{
+    [SerializeField]
+    private float m_moveSpeed = 5.0f;
+
+    void OnTriggerStay(Collider other) {
+        var body = other.gameObject.GetComponent<Rigidbody>();
+
+        if (body != null)
+        {
+            Vector3 add = transform.right * m_moveSpeed * Time.deltaTime;
+
+            body.MovePosition(other.transform.position + add);
+        }
+    }
+}
