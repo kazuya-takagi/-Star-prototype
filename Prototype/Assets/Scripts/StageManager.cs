7236198e901def97d60b8ca74f9ac98a10279f84
DESKTOP-CCNQVSV <yokohama.vcp@gmail.com>
2019-03-20 18:09:47 +0900

    Add PrototypeData

---
+++ Prototype/Assets/Scripts/StageManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    // ステージ用のプレハブを指定します。
+    [SerializeField]
+    GameObject[] stagePrefabs;
+    // ブロック用のプレハブを指定します。
+    [SerializeField]
+    GameObject[] blockPrefabs;
+    // ステージの縦横サイズを指定します。
+    [SerializeField]
+    Vector2Int size;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (var yIndex = 0; yIndex < size.y; yIndex++) {
+            for (var xIndex = 0; xIndex < size.x; xIndex++) {
+                // 偶数行
+                if (yIndex % 2 == 0) {
+                    var block = Instantiate(stagePrefabs[xIndex % 2], transform);
+                    block.transform.position = new Vector3(xIndex, 0, yIndex);
+                }
+                // 奇数行
+                else {
+                    var block = Instantiate(stagePrefabs[(xIndex + 1) % 2], transform);
+                    block.transform.position = new Vector3(xIndex, 0, yIndex);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ StarProject/Assets/Script/Prototype/PlayerMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove: MonoBehaviour
+{
+
+    float speed = 5.0f;        //移動速度
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //右移動
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += transform.right * speed * Time.deltaTime;
+            
+        }
+        //左移動
+        else if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position -= transform.right * speed * Time.deltaTime;
+          
+        }
+
+        //奥移動
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+        }
+        //手前移動
+        else if (Input.GetKey(KeyCode.DownArrow)) {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+
+        }
+
+        //他の入力
+        else {
+        
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ok");
+    }
+}
